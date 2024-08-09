c1232f462a81886c64116922473cf52134ade820
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2024-08-08 22:33:09 -0400

    feat: add Header componet and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 export default function Home() {
-  return <Button>teste</Button>;
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Rodrigo!</h2>
+        <p>Quinta-feira, 08 de agosto</p>
+
+        <div className="mt-6 flex flex-row items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative w-full h-[150px] mt-6">
+          <Image alt="Agende nos melhores com FSW Barber" src="/banner-01.png" fill className="object-cover rounded-xl" />
+        </div>
+      </div>
+
+    </div>
+  )
 }
