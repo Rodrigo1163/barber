cbc6adfcb090e5b4a0b66101068d67dae770e5d1
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2024-08-10 13:05:21 -0400

    feat: add quick search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { SearchIcon } from "lucide-react";
+import { EyeIcon, FootprintsIcon, SearchIcon } from "lucide-react";
 import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
@@ -12,7 +12,11 @@ import BarbershopItem from "./_components/barbershop-item";
 export default async function Home() {
 
   const barbershops = await db.barbershop.findMany({})
-
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc"
+    }
+  })
   return (
     <div>
       <Header />
@@ -26,8 +30,38 @@ export default async function Home() {
             <SearchIcon />
           </Button>
         </div>
+
+        <div className="flex gap-3 mt-6 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          <Button className="gap-2" variant="secondary">
+            <Image src="/cabelo.svg" width={16} height={16} alt="cabelo" />
+            Cabelo
+          </Button>
+          <Button className="gap-2" variant="secondary">
+            <Image src="/barba.svg" width={16} height={16} alt="barba" />
+            Barba
+          </Button>
+          <Button className="gap-2" variant="secondary">
+            <Image src="/acabamento.svg" width={16} height={16} alt="acabamento" />
+            Acabamento
+          </Button>
+          {/* mais */}
+          <Button className="gap-2" variant="secondary">
+            <FootprintsIcon size={16} />
+            Pézinho
+          </Button>
+          <Button className="gap-2" variant="secondary">
+            <EyeIcon size={16} />
+            Sobrancelha
+          </Button>
+
+        </div>
         <div className="relative w-full h-[150px] mt-6">
-          <Image alt="Agende nos melhores com FSW Barber" src="/banner-01.png" fill className="object-cover rounded-xl" />
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="object-cover rounded-xl"
+          />
         </div>
         <h2 className="text-xs font-bold uppercase text-gray-400 mt-6 mb-3">
           Agendamento
@@ -63,8 +97,29 @@ export default async function Home() {
               barbershop={barbershop}
             />)}
         </div>
+
+        <h2 className="text-xs font-bold uppercase text-gray-400 mt-6 mb-3">
+          populares
+        </h2>
+
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map(barbershop =>
+            <BarbershopItem key={barbershop.id}
+              barbershop={barbershop}
+            />)}
+        </div>
+
+
+
       </div>
 
+      <footer>
+        <Card >
+          <CardContent className="py-6 px-5">
+            <p className="text-sm text-gray-400">@ 2023 Copyright <span className="font-bold">FWS Barber</span> </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
